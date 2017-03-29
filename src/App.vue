a77f20912f9d05ebaefdc9ac8f004061447f1abb
cheerqjy <cheerqjy@163.com>
2017-03-29 13:13:37 +0800

    elementUI 测试

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <el-button type="danger">按钮</el-button>
+    <el-button type="primary">按钮</el-button>
+    <Tabs></Tabs>
+    <Loading></Loading>
+  </div>
+</template>
+
+<script>
+import Tabs from './components/Tabs.vue'
+import Loading from './components/Loading.vue'
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components:{
+    Tabs,
+    Loading
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
